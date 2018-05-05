ab498383244b33160487aaeeb41b7a670f8a7d27
augalde <augalde@gmail.com>
2018-05-05 17:02:55 -0600

    Init Base, interfaces and init class for generic

---
+++ MyUnitOfWork/MyUnitOfWorkDAL/Repository/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnitOfWorkDAL.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext dbContext;
+        private DbSet<TEntity> dbSet = null;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("dbcontext","dbContext is mandatory to use GenericRepository");
+            }
+
+            this.dbContext = dbContext;
+            dbSet = this.dbContext.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> FindAll(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindSingle(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyUnitOfWork/MyUnitOfWorkDAL/Repository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnitOfWorkDAL.Repository
+{
+    public interface IGenericRepository<TEntity> : IRepository
+    {
+        TEntity GetByID(object id);
+        IQueryable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Attach(TEntity entity);
+        void Delete(TEntity entity);
+
+        IQueryable<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate);
+        TEntity FindSingle(Expression<Func<TEntity, bool>> predicate);
+
+    }
+}
